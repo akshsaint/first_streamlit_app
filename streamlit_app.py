338e8477848502214fcdd802a91cdb07a7f1b360
akshsaint <96130999+akshsaint@users.noreply.github.com>
2022-10-20 13:56:00 +0530

    Update streamlit_app.py
    imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,9 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Vada Sambar')
 streamlit.text('idli Sambar')
 streamlit.text('Dosa')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
